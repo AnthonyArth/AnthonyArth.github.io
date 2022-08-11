9950f22f254aa4f1209c0c178ddb705b9eb42fb2
AnthonyArth <anthonyarthadjaya@gmail.com>
2022-08-11 23:20:04 +0800

    finished the quiz

--- webdeva2/js/quizPlay.js
+++ webdeva2/js/quizPlay.js
@@ -1,11 +1,32 @@
 //useful functions
 //get a random integer
 function GetRandomInteger(min, max) {
+    //error checking
+    if(Number.isInteger(min) == false)
+    {
+        throw new Error("min cannot be a non integer")
+    }
+    else if(Number.isInteger(max) == false)
+    {
+        throw new Error("max cannot be a non integer")
+    }
+    else if(min > max)
+    {
+        throw new Error("min cannot exceed max")
+    }
+    //return a random int
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 //shuffle an array
 function Shuffle(array){
+    //check if array is an array
+    if(Array.isArray(array) == false)
+    {
+        //throw error if so
+        throw new Error("Cannot shuffle non array")
+    }
+
     //we use the Fisher-Yates shuffle
     let currentIndex = array.length,  randomIndex;
 
@@ -52,6 +73,7 @@ class CQuestion{
     }
 
     SetAnswer(answer){
+        //we want anser to be an array since there may be multiple answers
         //check if answer is an array
         if(Array.isArray(answer) == false)
         {
@@ -154,7 +176,7 @@ let questionsToAsk = new Array;
 let currentQuestion;
 let questionNumber;
 let currentQuizNumber;
-let questionIndicator
+let questionIndicator;
 
 function Init(){
     //init elements to put in variables
@@ -172,41 +194,140 @@ function Init(){
     "A CPU is a processor that executes instructions from a computer.",
     "A CPU is Central Processing Unit.",
     "A CPU is electronic circuitry that processes data to output results."]
-    ))
+    ));
 
-    questions.push(new CMultipleChoiceQuestion("What is a GPU", //question
+    questions.push(new CMultipleChoiceQuestion("What is a GPU?", //question
     ["A GPU is a specialized electronic circuit designed to manipulate and alter memory to accelerate the creation of images in a frame buffer intended for output to a display device."], //answer
     //choices
     ["A GPU is a specialized electronic circuit designed to manipulate and alter memory to accelerate the creation of images in a frame buffer intended for output to a display device.",
     "A GPU is a processor that generates images.",
     "A GPU is Graphics Processing Unit.",
     "A GPU is a specialized electronic circuit designed to manipulate and alter memory to create images in a frame buffer intended for output to a display device."]
-    ))
+    ));
 
-    questions.push(new CMultipleChoiceQuestion("What was considered to be the first computer", //question
+    questions.push(new CMultipleChoiceQuestion("What was considered to be the first computer?", //question
     ["The babbage difference engine."], //answer
     //choices
     ["The babbage difference engine.",
     "The abacus.",
     "The universal turing machine.",
     "The differential analyzer."]
-    ))
+    ));
 
+    questions.push(new CMultipleChoiceQuestion("Most microprocessors are made from transistors. True or False.", //question
+    ["True"], //answer
+    //choices
+    ["True",
+    "False"]
+    ));
+
+    //delete the current quiz storage used if quit abruptly
+    unloadEventListener = window.addEventListener("unload", RemoveLocal);
+
+    //shuffle questions
+    Shuffle(questions);
+    //set the first question
     currentQuestion = questions[0];
     questionNumber = 1;
     SetQuestion(currentQuestion);
 }
 
-function EndGame()
+function RemoveLocal()
 {
-    quizBody.innerHTML = "";
-    let node = document.createElement("h1");
-    node.innerHTML = "Your Score: " + localStorage.getItem(currentQuizNumber);
-    quizBody.appendChild(node);
+    localStorage.removeItem(currentQuizNumber);
 }
 
+//function that sets up the end of quiz screen
+function EndGame()
+{
+    //create the result array
+    let resultArray = localStorage.getItem(currentQuizNumber).split(",");
+
+    //make quiz body into input field
+    quizBody.innerHTML = "<p>Press enter to submit name</p><form id='form'><input id='name' type='text'></input></form>";
+
+    document.querySelector("#form").addEventListener("submit", (f) => {
+        f.preventDefault(); //prevent default behaviour of submit event
+        window.removeEventListener("unload", RemoveLocal)
+        localStorage.setItem(currentQuizNumber, localStorage.getItem(currentQuizNumber) + "," + document.querySelector("#name").value)
+        window.location.href = "./quiz.html";
+    });
+
+    //create the score indicator
+    let node = document.createElement("h1");
+
+    let score = 0;
+    //get the score
+    for(let i = 0; i < resultArray.length; ++i)
+    {
+        score += parseInt(resultArray[i]);
+    }
+    //set the score
+    node.innerHTML = "Your Score: " + score + "/" + resultArray.length;
+    quizBody.appendChild(node);
+
+    //create the full results view
+    let resultBox = document.createElement("div");
+    resultBox.classList.add("flexbox", "flex-row", "flex-center");
+    resultBox.id = "results-full";
+    for(let i = 0; i < questions.length; ++i)
+    {
+        node = document.createElement("div");
+        node.classList.add("results-box");
+        if(resultArray[i] === "0")
+        {
+            node.classList.add("results-box-wrong")
+        }
+        else
+        {
+            node.classList.add("results-box-correct")
+        }
+
+        node.innerHTML = "<div class='results-box-number flexbox flex-center'>" + (i + 1)+ "</div>"; //can also add elements like this
+        resultBox.appendChild(node);
+    }
+    quizBody.appendChild(resultBox);
+
+    //create the full results collapse button;
+    node = document.createElement("div");
+    node.classList.add("results-collapse-button");
+    //add event listener
+    node.addEventListener("click", () => {
+        //to make the results appear
+        if(resultBox.classList.contains("slide-in"))
+        {
+            resultBox.classList.remove("slide-in");
+        }
+        else
+        {
+            resultBox.classList.add("slide-in");
+        }
+
+        //to make the button rotate
+        if(node.classList.contains("rotate-180"))
+        {
+            node.classList.remove("rotate-180");
+        }
+        else
+        {
+            node.classList.add("rotate-180");
+        }
+    });
+    quizBody.insertBefore(node, resultBox);
+    
+}
+
+//function that sets the current question
 function SetQuestion()
 {
+    //delete buttons if there are any
+    let quizBodyInitialLength = quizBody.children.length;
+    for(let i = 0; i < quizBodyInitialLength - 2 ; ++i)
+    {
+        quizBody.removeChild(quizBody.children[1]);
+    }
+
+    //get the choices
     let choices = currentQuestion.GetChoices();
     if(!(currentQuestion instanceof CQuestion))
     {
@@ -273,13 +394,6 @@ function SetQuestion()
             //if not after last question
             if(questionNumber <= questions.length)
             {
-                //delete buttons
-                let quizBodyInitialLength = quizBody.children.length;
-                for(let i = 0; i < quizBodyInitialLength - 2 ; ++i)
-                {
-                    quizBody.removeChild(quizBody.children[1]);
-                }
-
                 //go to the next question
                 currentQuestion = questions[questionNumber - 1];
                 SetQuestion(currentQuestion);
@@ -295,4 +409,5 @@ function SetQuestion()
     document.querySelector("#question-indicator").innerHTML = questionNumber + "/" + questions.length;
 }
 
+//run init on DOMContentLoaded
 document.addEventListener("DOMContentLoaded", Init);
---
+++ webdeva2/js/quizScores.js
@@ -0,0 +1,46 @@
+let quizBody = document.querySelector("#quiz-body");
+
+let temp
+if(localStorage.length > 0)
+{
+    let dataArray; //this will contain data from one of the local storage nodes
+    let score; //this will contain the score from one of the local storage nodes
+    for(let i = 0; i < localStorage.length; ++i)
+    {
+        //init the dataArray
+            dataArray = localStorage.getItem("ans" + i);
+            dataArray = dataArray.split(",");
+
+            //create the score-box
+            temp = document.createElement("div");
+            temp.classList.add("flexbox","flex-row", "score-box")
+            quizBody.appendChild(temp);
+
+            //create the score-box-number
+            let node = document.createElement("div");
+            node.classList.add("score-box-number", "flexbox", "flex-center");
+            node.innerHTML = i + 1;
+            temp.appendChild(node);
+
+            //calculate score
+            score = 0;
+            for(let x = 0; x < dataArray.length - 1; ++x)
+            {
+                score += parseInt(dataArray[x]);
+            }
+
+            //create the score-box-content
+            node = document.createElement("div");
+            node.classList.add("score-box-content");
+            node.innerHTML = "Name: " + dataArray[dataArray.length - 1] + " Score: " + score;
+            temp.appendChild(node);
+    }
+}
+else
+{
+    //create the a h1 that says NA at center of screen
+    temp = document.createElement("div");
+    temp.classList.add("align-center-screen");
+    temp.innerHTML = "NA";
+    quizBody.appendChild(temp);
+}
